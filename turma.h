8e4f472eb077a6ce14a6e02b1a1cfcf4e2829807
Gabriel <129018468+GabrielxBarreto@users.noreply.github.com>
2025-06-18 00:22:30 -0300

    criação da struct turma

---
+++ turma.h
@@ -0,0 +1,16 @@
+#include "aluno.h"
+#include "professor.h"
+typedef struct 
+{
+    Professor professor;
+    Aluno listaDeAlunosInscritos[40];
+    int codigoMateria;
+    int cargaHoraria;
+    int codigoDaTurma;
+}Turma;
+static int codrefH = 456;
+static int codrefT = 369;
+int criarTurma(Professor professor){
+    
+    return 0;
+}
