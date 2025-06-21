9531f26a7ab3e1806e576747c4423d06bbde57c7
alanfolv <alanfoliveira366@gmail.com>
2025-06-21 20:45:04 -0300

    pequenos ajustes

--- funcoes.c
+++ funcoes.c
@@ -59,14 +59,14 @@ void somaNotasPorAluno(Turma *turma, Aluno alunos[], int totalAlunos){
 //Requisito Funcional 3 : Calcular a soma das notas por avaliaçao aplicada
 void somaNotasPorAvaliacao(Turma *turma, Aluno alunos[], int totalAlunos){
     for(int a=0; a <turma->nA ; a++){
-        float soma == 0;
+        float soma  = 0;
         for (int i = 0; i<40;i++){
             int matricula = turma->listaDeAlunosInscritos[i];
             if(matricula == 0)continue;
 
             for(int j = 0; j< totalAlunos; j++){
                 if(alunos[j].matricula == matricula){
-                    soma += alunos[j].notas[a]
+                    soma += alunos[j].notas[a];
                     break;
                 }
             }
@@ -76,7 +76,7 @@ void somaNotasPorAvaliacao(Turma *turma, Aluno alunos[], int totalAlunos){
 }
 
 //Função que gera um relatório geral do aluno
-//Requisito funcional 4 : Gerar um relatório geral de cada aluno
+//Requisito funcional 5 : Gerar um relatório geral de cada aluno
 void gerarRelatorioGeralDoAluno(Aluno aluno){
     printf("Relatório do aluno : %s (Matricula : %d)\n", aluno.nome , aluno.matricula);
     printf("Notas : ");
